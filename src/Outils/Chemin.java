98e4d09c2d5b2f389ab2b3437c730a951d2ce080
darouyy <51452432+darouyy@users.noreply.github.com>
2021-05-07 14:49:19 +0000

    l'ajout du score

--- src/Outils/Chemin.java
+++ src/Outils/Chemin.java
@@ -1,7 +1,43 @@
 package Outils;
+/*
+	Cette classe sert à trouver des points solution donnée est proche de la 
+	solution optimale du problème souhaité. 
+	Il détermine l'adéquation d'une solution.
+*/
 
-// cette classe sert à calculer le coût d'un chemin
 public class Chemin {
 
+	
+	private int [] villeParcourus; // ensemble des villes déjà visité
+	private double score = -1; 
+	
+	public Chemin(int[] villeParcourus)
+	{
+		this.villeParcourus = villeParcourus;
+	}
+
+	public Chemin(int nbreVilleParcourus)
+	{
+		//initialisation du tableaux
+		int[] villerempli = new int[nbreVilleParcourus];
+		
+		
+		//créer un chemin aléatoire  
+		for(int ville = 0; ville < nbreVilleParcourus; ville++)
+		{
+			villeParcourus[ville] = ville; 
+		}
+		
+		this.villeParcourus = villerempli;
+	}
+	
+	public int[] getVilleParcourus() {
+		return villeParcourus;
+	}
+
+	public void setVilleParcourus(int[] villeParcourus) {
+		this.villeParcourus = villeParcourus;
+	}
+	
 	
 }
