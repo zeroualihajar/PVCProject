8441d2b040c527475b82ed5fb12d5f73d258b6b0
Hajar ZEROUALI <61935735+zeroualihajar@users.noreply.github.com>
2021-05-07 15:56:33 +0000

    update population.java

--- src/Outils/Solutions.java
+++ src/Outils/Solutions.java
@@ -1,24 +1,133 @@
 package Outils;
 
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Random;
 
 public class Solutions {
 	
-	private Chemin Chemins[]; //---- liste des chemins possibles
-	private double score = -1;
+	private Chemin chemins[];     //---- liste des chemins possibles
+	private double score = -1;    //---- represente le fitness de chaque chemin
 	
 	
-	//------ Constructer -> Initialisation
+
+	//------ Constructer -> Initialisation de l'ensembles des solutions (Chemins)
 	public Solutions(int taille) {
-		super();
-		this.Chemins = new Chemin[taille];
+		this.chemins = new Chemin[taille];
 	}
 	
-	public Solutions(int taille, int tailleSol) {
-		this.Chemins = new Chemin[taille];
+	
+	public Solutions(int taille, int tailleVilleParcourus) {
+		//------ Initialistation de l'ensembles des solutions
+		this.chemins = new Chemin[taille];
 		
 		
+		//------ Rempllissage de l'ensemble des solutions
+		for(int i = 0; i< taille; i++) {
+			
+			//----- Creation d'un chemin
+			Chemin chemin = new Chemin(tailleVilleParcourus);
+			
+			//----- Ajouter le chemin a l'ensemble des solutions
+			this.chemins[i] = chemin;
+			
+		}
 
 	}
+
+
+	//------- Getters & Setters
+	public Chemin[] getChemins() {
+		return chemins;
+	}
+
+
+	public void setChemins(Chemin[] chemins) {
+		chemins = chemins;
+	}
+
+
+	public double getScore() {
+		return score;
+	}
+
+
+	public void setScore(double score) {
+		this.score = score;
+	}
+	
+	//----- la taille de l'ensemble des solutions
+	public int taille() {
+		return this.chemins.length;
+	}
+	
+	//----- ????????????????????????
+	public Chemin setChemin(Chemin chemin, int offset) {
+		return this.chemins[offset] = chemin;
+	}
+	
+	
+	//----- returner le chemin ???????????
+	public Chemin getChemin(int offset) {
+		return chemins[offset];
+	}
+	
+	
+	//-------- Trouver le chemin le plus apte
+	public Chemin CheminApte(int offset ) {
+		
+		//----- Ordonner l'ensembles des chemins (solutions) selon le score
+		
+		Arrays.sort(this.chemins, new Comparator<Chemin>() {
+			
+			@Override
+			public int compare(Chemin ch1, Chemin ch2) {
+				if(ch1.getScore() > ch2.getScore()) 
+					return -1; 
+							
+				else if(ch1.getScore() < ch2.getScore()) 
+					return 1;
+							
+				return 0;
+				}
+			});
+		
+		return chemins[offset];
+	}
+	
+	
+	//----- Melanger l'enseble des solutions d'une maniere aleatoire
+	public void shuffle() {
+		Random random = new Random();
+		for(int i = this.taille() - 1; i > 0; i--) {
+			
+			int j = random.nextInt(i+1);
+			Chemin chemin = this.chemins[j];
+			this.chemins[j] = this.chemins[i];
+			this.chemins[i] = chemin;
+		}
+	}
+		
+				
+		
+		
+	
+		
+	
+	
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	
 
 }
