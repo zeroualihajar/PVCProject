1008eea6136e90966b982fb9dddaa04cc8e21add
Hajar ZEROUALI <61935735+zeroualihajar@users.noreply.github.com>
2021-05-07 14:05:40 +0000

    update ville.java

--- src/Outils/Ville.java
+++ src/Outils/Ville.java
@@ -1,15 +1,14 @@
 package Outils;
 
 public class Ville {
-	
+
 	private int idVille;
 	private String nomVille;
 	private double posX;
 	private double posY;
 	private boolean visite;
-	
-	
-	//------- Constructeur ------
+
+	// ------- Constructeur ------
 	public Ville(int idVille, String nomVille, double posX, double posY, boolean visite) {
 		super();
 		this.idVille = idVille;
@@ -19,58 +18,53 @@ public class Ville {
 		this.visite = visite;
 	}
 
-
-	//------- Getetrs & Setters -------
+	// ------- Getetrs & Setters -------
 	public int getIdVille() {
 		return idVille;
 	}
-
 
 	public void setIdVille(int idVille) {
 		this.idVille = idVille;
 	}
 
-
 	public String getNomVille() {
 		return nomVille;
 	}
-
 
 	public void setNomVille(String nomVille) {
 		this.nomVille = nomVille;
 	}
 
-
 	public double getPosX() {
 		return posX;
 	}
-
 
 	public void setPosX(double posX) {
 		this.posX = posX;
 	}
 
-
 	public double getPosY() {
 		return posY;
 	}
-
 
 	public void setPosY(double posY) {
 		this.posY = posY;
 	}
 
-
 	public boolean isVisite() {
 		return visite;
 	}
 
-
 	public void setVisite(boolean visite) {
 		this.visite = visite;
 	}
-	
-	
-	
-	
+
+	// ------- distance entre deux villes
+	public double getDistance(Ville ville) {
+
+		double x = Math.abs(this.posX - ville.getPosX());
+		double y = Math.abs(this.posY - ville.getPosY());
+		return Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2));
+	}
+
 }
