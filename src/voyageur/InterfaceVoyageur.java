4f6cabbe1a0a2f5a3736fdb74ffb4eef5e865256
Hajar ZEROUALI <61935735+zeroualihajar@users.noreply.github.com>
2021-05-08 13:26:58 +0000

    Interface Voyageur + MapPanel

---
+++ src/voyageur/InterfaceVoyageur.java
@@ -0,0 +1,105 @@
+package voyageur;
+
+import java.awt.Color;
+import java.awt.EventQueue;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.HashMap;
+import java.util.List;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+import jade.gui.GuiEvent;
+import outils.Ville;
+
+public class InterfaceVoyageur extends JFrame {
+	private static final long serialVersionUID = 1L;
+	
+	private JPanel contentPane;
+	public JPanel mapPanel;
+	public MapPanel map;
+	
+	private AgentVoyageur agentVoyageur;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					InterfaceVoyageur frame = new InterfaceVoyageur();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+
+	public InterfaceVoyageur() {
+		setTitle("PVC");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 644, 412);
+		setResizable(false);
+		contentPane = new JPanel();
+		contentPane.setBackground(new Color(248, 248, 255));
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+
+		mapPanel = new JPanel();
+		mapPanel.setBackground(new Color(255, 245, 238));
+		mapPanel.setBounds(10, 62, 500, 300);
+		contentPane.add(mapPanel);
+		mapPanel.setLayout(null);
+		
+		map = new MapPanel();
+		map.setBounds(245, 5, 10, 10);
+		map.setBounds(0, 0, map.WIDTH_MAP, map.HEIGHT_MAP);
+        mapPanel.add(map);
+        map.repaint();
+
+		JButton getCheminBtn = new JButton("Calculer");
+		getCheminBtn.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				System.out.println("On va commencer le calcul :)");
+				GuiEvent gev = new GuiEvent(contentPane.getUI(), 1);
+				List<Ville> villes = map.villes;
+				java.util.Map<String, Object> params = new HashMap<>();
+				params.put("VillesV1", villes);
+
+				gev.addParameter(params);
+				agentVoyageur.onGuiEvent(gev);
+			}
+		});
+		getCheminBtn.setBounds(10, 13, 113, 39);
+		contentPane.add(getCheminBtn);
+
+		/*
+		 * mapPanel.addMouseListener(new MouseAdapter() {
+		 * 
+		 * @Override public void mouseClicked(final MouseEvent e) { String retour =
+		 * JOptionPane.showInputDialog("Saisir le nom de la ville : "); final int x =
+		 * e.getX(); final int y = e.getY();
+		 * 
+		 * try { imageVille = ImageIO.read(new File("images/" + "favicon.ico")); //
+		 * this.VilleImageCenter = new Dimension(this.VilleImage.getWidth() / 2,
+		 * this.VilleImage.getHeight() / 2); } catch (final IOException ex) { imageVille
+		 * = null; // this.VilleImageCenter = new Dimension(0, 0); }
+		 * 
+		 * villes.add(new Ville(villes.size() + 1, retour, x, y, false));
+		 * System.out.println("Ville : " + retour + "\n" + "(x,y) : (" + e.getX() + ", "
+		 * + e.getY() + ")\n" + "Nbre de villes : " + villes.size());
+		 * 
+		 * paintVilles(); repaint(); } });
+		 */
+	}
+}
---
+++ src/voyageur/MapPanel.java
@@ -0,0 +1,131 @@
+package voyageur;
+
+import java.awt.BasicStroke;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Image;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.swing.ImageIcon;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+
+import outils.Ville;
+
+public class MapPanel extends JPanel {
+	private static final long serialVersionUID = 1L;
+
+	public List<Ville> villes = new ArrayList<Ville>();
+	public List<Ville> chemins = new ArrayList<Ville>();//Route
+
+	public Image imageVille;
+	public int WIDTH_MAP = 500;
+	public int HEIGHT_MAP = 300;
+	public Image imageBackground;
+
+	/**
+	 * Create the panel.
+	 */
+	public MapPanel() {
+		setBackground(new Color(245, 255, 250));
+		this.setBounds(0, 0, WIDTH_MAP, HEIGHT_MAP);
+		setLayout(null);
+
+		this.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(final MouseEvent e) {
+				String retour = JOptionPane.showInputDialog("Saisir le nom de la ville : ");
+				final int x = e.getX();
+				final int y = e.getY();
+
+				try {
+					// imageVille = new
+					// ImageIcon(MapPanel.class.getResource("/images/city"+(villes.get(villes.size()-1).getIdVille()+1)+".png")).getImage();//ImageIO.read(new
+					// File("/images/" + "corona.jpeg"));
+					imageVille = new ImageIcon(MapPanel.class.getResource("/images/city1.png")).getImage();
+					// System.out.println("/images/city"+(villes.get(villes.size()-1).getIdVille()+1)+".png");
+//					this.VilleImageCenter = new Dimension(this.VilleImage.getWidth() / 2, this.VilleImage.getHeight() / 2);
+				} catch (final Exception ex) {
+					imageVille = null;
+//					this.VilleImageCenter = new Dimension(0, 0);
+				}
+
+				villes.add(new Ville(villes.size() + 1, retour, x, y, false));
+
+				System.out.println("Ville : " + retour + "\n(x,y) : (" + e.getX() + ", " + e.getY()
+						+ "\nNbre de villes : " + villes.size() + "\n");
+				repaint();
+			}
+		});
+	}
+
+	@Override
+	public void paint(Graphics g) {
+		Graphics2D g2d = (Graphics2D) g;
+		super.paint(g2d);
+
+		for(Ville ville : villes) {
+			paintVille(g2d, ville);
+		}
+	}
+
+	/* ----- L'arrière plan (la carte) du panel ----- */
+	@Override
+	protected void paintComponent(Graphics g) {
+		Graphics2D g2d = (Graphics2D) g;
+		super.paintComponent(g2d);
+
+		this.imageBackground = new ImageIcon(MapPanel.class.getResource("/images/map.png")).getImage();
+
+//		int width = imageBackground.getWidth(this)/2;
+//		int height = imageBackground.getHeight(this)/2;
+
+		if (imageBackground != null) {
+//			int x = this.getParent().getWidth() / 2 - width;
+//			int y = this.getParent().getHeight() / 2 - height;
+			g.drawImage(imageBackground, 0, 0, this);
+		}
+	}
+
+	/* ----- Dessin des villes ----- */
+	public void paintVille(Graphics2D g, Ville ville) {
+		int x = ville.getPosX();
+		int y = ville.getPosY();
+//		int centerX = this.VilleImageCenter.width;
+//		int centerY = this.VilleImageCenter.height;
+
+		// Black background
+		g.setColor(Color.RED);
+		g.fillOval(x - 45, y - 25, 10 * 2, 10 * 2);
+
+		// ville image
+		g.drawImage(imageVille, x - 45, y - 25, null);
+
+		// Number background
+//		g.setColor(Color.WHITE);
+//		g.fillOval(x + 3, y + 3, 14, 14);
+
+		// Number
+		g.setColor(Color.WHITE);
+
+		g.drawString(ville.getNomVille(), x + 1, y + 17);
+	}
+
+	/* ----- Dessin d'une route entre deux villes ----- */
+	public void paintChemin(Graphics2D g, Ville ville1, Ville ville2) {
+		if (ville1 != null && ville2 != null) {
+			int x1 = ville1.getPosX();
+			int y1 = ville1.getPosY();
+			int x2 = ville2.getPosX();
+			int y2 = ville2.getPosY();
+
+			g.setColor(Color.RED);
+			g.setStroke(new BasicStroke(2));
+			g.drawLine(x1, y1, x2, y2);
+		}
+	}
+}
