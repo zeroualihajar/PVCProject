3716abcadb7d077b0edb3536b594dccfb34e699c
Hajar ZEROUALI <61935735+zeroualihajar@users.noreply.github.com>
2021-05-08 12:57:12 +0000

    Update AgentVoyageur.java

--- src/voyageur/AgentVoyageur.java
+++ src/voyageur/AgentVoyageur.java
@@ -1,16 +1,64 @@
 package voyageur;
+import Outils.Ville;
+import jade.core.behaviours.CyclicBehaviour;
+import jade.core.behaviours.ParallelBehaviour;
 import jade.gui.GuiAgent;
+import jade.lang.acl.ACLMessage;
+import jade.lang.acl.MessageTemplate;
 
 public class AgentVoyageur extends GuiAgent {
 	
-	private Outils.TSPUI agentInterface;
+	private Outils.TSPUI interfaceAgent;
 	
 	
 	protected void setup() {
 		System.out.println("Bienvenu ! C'est le voyageur Agent");
 		
-		agentInterface = new Outils.TSPUI();
-		agentInterface
+		interfaceAgent = new Outils.TSPUI();
+		interfaceAgent.createAndDisplay();
+		interfaceAgent.setAgentVoyageur(this);
+		
+		System.out.println("Le Voyageur est pret !");
+		
+		ParallelBehaviour parallelBehaviour = new ParallelBehaviour();
+		addBehaviour(parallelBehaviour);
+		
+		parallelBehaviour.addSubBehaviour(new CyclicBehaviour() {
+			
+			private int compteur;
+			@Override
+			public void action() {
+				++compteur;
+				
+				MessageTemplate message1  = MessageTemplate.and(MessageTemplate.MatchPerformative(ACLMessage.REQUEST), MessageTemplate.MatchOntology("Il marche"));
+				ACLMessage acl1 = receive(message1);
+				
+				MessageTemplate message2 = MessageTemplate.and(MessageTemplate.MatchPerformative(ACLMessage.INFORM), MessageTemplate.MatchOntology("Ordonner les villes"));
+				ACLMessage acl2 = receive(message2);
+				
+				
+				if(acl1 !=null) {
+					System.out.println("L'emetteur de message : " +acl1.getSender());
+					System.out.println("Le destinataire de message : "+acl1.getInReplyTo());
+					System.out.println("L'acte de communication : " +acl1.getPerformative());
+					System.out.println("LE contenu de message : " +acl1.getContent());
+					System.out.println("Langage : " +acl1.getLanguage());
+					System.out.println("L'ontology : " +acl1.getOntology());
+						
+				}
+				else if(acl2 != null) {
+					Ville[] villeOrdonnee = (Ville[]) acl2.getContentObject();
+					interfaceAgent.getBar().villes = villeOrdonnee;
+//					interfaceAgent.panelMap().setVillesro
+							
+					
+				}
+				
+			}
+		}
+				
+				
+				);
 		
 	}
 
